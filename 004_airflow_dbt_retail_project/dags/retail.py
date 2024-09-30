b1a68508448aaaa2ba106f9bc0bcd8c7e4781485
Piotr <pmackowka@gmail.com>
2024-09-30 15:35:50 +0200

    soda

--- 004_airflow_dbt_retail_project/dags/retail.py
+++ 004_airflow_dbt_retail_project/dags/retail.py
@@ -1,8 +1,13 @@
 from airflow.decorators import dag, task
 from datetime import datetime
 
+from astro import sql as aql
+from astro.files import File
+from astro.sql.table import Table, Metadata
+from astro.constants import FileType
+
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator # operator do przesyłania plików z lokalnego systemu plików do GCS
-from airflow.providers.google.suite.transfers.local_to_drive import LocalFilesystemToGoogleDriveOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
 
 @dag(
     start_date=datetime(2024, 1, 1),
@@ -15,12 +20,38 @@ def retail():
     upload_csv_to_gcs = LocalFilesystemToGCSOperator(
         task_id='upload_csv_to_gcs',
         src='include/dataset/online_retail.csv',
-        dst='003_loading_data_from_www_page_to_bq/online_retail.csv',
+        dst='004_airflow_dbt_retail_project/online_retail.csv',
         bucket='data_stream_gcs',
         gcp_conn_id='gcp',
         mime_type='text/csv',
     )
 
-retail()
+    create_temp_dataset = BigQueryCreateEmptyDatasetOperator(
+		task_id='create_temp_dataset',
+		dataset_id='temp',
+		gcp_conn_id='gcp',
+	)
 
-#  airflow tasks test retail upload_csv_to_gcs 2024-01-01
+    gcs_to_raw = aql.load_file(
+		task_id='gcs_to_raw',
+		input_file=File(
+			'gs://data_stream_gcs/004_airflow_dbt_retail_project/online_retail.csv',
+			conn_id='gcp',
+			filetype=FileType.CSV,
+		),
+		output_table=Table(
+			name='raw_invoices',
+			conn_id='gcp',
+			metadata=Metadata(schema='temp')
+		),
+		use_native_support=False,
+	)
+
+    # @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    # def check_load(scan_name='check_load', checks_subpath='sources'):
+    #     from include.soda.check_function import check
+    #     return check(scan_name, checks_subpath)
+
+    # check_load()
+
+retail()
