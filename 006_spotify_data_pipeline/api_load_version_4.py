16145313d624e20fb3c14ae9745985589375d0d6
Piotr <pmackowka@gmail.com>
2024-10-14 20:16:07 +0200

    006_spotify_data_pipeline/api_load_version_4

--- 006_spotify_data_pipeline/api_load_version_4.py
+++ 006_spotify_data_pipeline/api_load_version_4.py
@@ -1,8 +1,3 @@
-'''
-Transform - chodzi o weryfikację czy dane są dobre np. plik JSON nie jest pusty, albo nie ma tam duplikatów.
-
-Weryfikacja (Integralność danych) w poniższym kodzie sprowadza się do funkcji check_if_valid_data (nie wywołana jeszcze)'''
-
 import sqlalchemy
 import pandas as pd
 from sqlalchemy.orm import sessionmaker
@@ -35,7 +30,6 @@ def check_if_valid_data(df: pd.DataFrame) -> bool:
         if timestamp_date != yesterday:
             print(f"Warning: A song with timestamp {timestamp} is not from yesterday.")
 
-    # Jeśli wszystkie warunki są spełnione
     return True
 
 if __name__ == "__main__":
@@ -74,4 +68,30 @@ if __name__ == "__main__":
 
     song_df = pd.DataFrame(song_dict, columns = ["song_name", "artist_name", "played_at", "timestamp"])
 
-    print(song_df)
+    if check_if_valid_data(song_df):
+        print("Dane prawidłowe, przejdź do etapu ładowania (Load)")
+
+    engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+    conn = sqlite3.connect('my_played_tracks.sqlite')
+    cursor = conn.cursor()
+
+    sql_query = """
+    CREATE TABLE IF NOT EXISTS my_played_tracks(
+        song_name VARCHAR(200),
+        artist_name VARCHAR(200),
+        played_at VARCHAR(200),
+        timestamp VARCHAR(200),
+        CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
+    )
+    """
+
+    cursor.execute(sql_query)
+    print("Pomyślnie otwarto bazę danych")
+
+    try:
+        song_df.to_sql("my_played_tracks", engine, index=False, if_exists='append')
+    except:
+        print("Dane już istnieją w bazie danych")
+
+    conn.close()
+    print("Zamknij pomyślnie bazę danych")
